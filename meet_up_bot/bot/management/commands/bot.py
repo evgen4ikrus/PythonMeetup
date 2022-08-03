cc0db913e89d4466b50fc2c2f79f4935a58851a7
MaksAnikeev <105814349+MaksAnikeev@users.noreply.github.com>
2022-08-03 19:23:35 +0300

    Del not usefull files

--- meet_up_bot/bot/management/commands/bot.py
+++ meet_up_bot/bot/management/commands/bot.py
@@ -89,7 +89,7 @@ def button(update, context):
     elif q.data == 'Program_2':
         return everest_keyboard(update, context)
     elif q.data == 'Program_3':
-        return alps_keyboard
+        return alps_keyboard(update, context)
     elif q.data == 'Program_4':
         return finish_keyboard(update, context)
     elif q.data == 'Program_5':
@@ -132,45 +132,6 @@ def button(update, context):
         return program_keyboard(update, context)
 
 
-# функция обработки текстовых сообщений
-def echo(update, context):
-    text = 'ECHO: ' + update.message.text 
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text=text)    
-
-# функция обработки команды '/caps'
-def caps(update, context):
-    if context.args:
-        text_caps = ' '.join(context.args).upper()
-        context.bot.send_message(chat_id=update.effective_chat.id, 
-                                text=text_caps)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, 
-                                text='No command argument')
-        context.bot.send_message(chat_id=update.effective_chat.id, 
-                                text='send: /caps argument')
-
-# функция обработки встроенного запроса
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Convert to UPPER TEXT',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
-
-# функция обработки не распознных команд
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text="Такой команды нет, попробуйте еще раз")
-
-
 class Command(BaseCommand):
 
     load_dotenv()
@@ -190,21 +151,7 @@ class Command(BaseCommand):
     button_handler = CallbackQueryHandler(button)
     dispatcher.add_handler(button_handler)
 
-    # обработчик текстовых сообщений
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
 
-    # обработчик команды '/caps'
-    caps_handler = CommandHandler('caps', caps)
-    dispatcher.add_handler(caps_handler)
-
-    # обработчик встроенных запросов 
-    inline_caps_handler = InlineQueryHandler(inline_caps)
-    dispatcher.add_handler(inline_caps_handler)
-
-    # обработчик не распознных команд
-    unknown_handler = MessageHandler(Filters.command, unknown)
-    dispatcher.add_handler(unknown_handler)
 
     # запуск прослушивания сообщений
     updater.start_polling()
