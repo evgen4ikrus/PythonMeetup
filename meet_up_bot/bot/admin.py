216966ec9e62df79888037ff6af8b365fb05a3ae
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-05 16:52:37 +0300

    Сделал удобную админку

--- meet_up_bot/bot/admin.py
+++ meet_up_bot/bot/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-
 from .models import Block, Speaker, Presentation, Flow, Flow_group
+
 
 @admin.register(Block)
 class ComplaintAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'start_time', 'end_time']
+    list_editable = ['start_time', 'end_time']
 
 
 @admin.register(Speaker)
 class ComplaintAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['full_name', 'job_title']
 
 
 @admin.register(Presentation)
 class ComplaintAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'block']
 
 
 @admin.register(Flow)
@@ -24,4 +25,5 @@ class ComplaintAdmin(admin.ModelAdmin):
 
 @admin.register(Flow_group)
 class ComplaintAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'flow', 'start_time', 'end_time']
+    list_editable = ['start_time', 'end_time']
